d55bdbb61b9d02bd67dac68c09417cb95b796cbf
Mark Oliver Roman <markoliverroman@Marks-MacBook-Pro.local>
2025-09-02 13:04:38 +0800

    updating 090203

--- components/reusable_components/CustomAvatar.jsx
+++ components/reusable_components/CustomAvatar.jsx
@@ -21,6 +21,7 @@ export default function CustomAvatar({
                 fill
                 style={{ objectFit: "cover" }}
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                unoptimized
             />
         </div>
     );
