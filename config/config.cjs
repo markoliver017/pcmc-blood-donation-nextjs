bcee49bf6e1805fc6385eb04a34087fd8e8bbdcd
markoliver017 <markoliver01728@gmail.com>
2025-10-07 10:56:50 +0800

    updating 100702

--- config/config.cjs
+++ config/config.cjs
@@ -1,6 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
+const path = require("path");
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const dotenv = require("dotenv");
-dotenv.config({ path: ".env.production" });
+// 1. Determine the environment (Sequelize CLI defaults to 'development')
+// The CLI will automatically match the section name ('development' or 'production')
+const NODE_ENV = process.env.NODE_ENV || "development";
+// 2. Construct the path to the correct .env file
+// This ensures it loads .env.development or .env.production
+const envPath = path.resolve(__dirname, `../.env.${NODE_ENV}`);
+dotenv.config({ path: envPath });
+
+console.log("envPath", envPath);
 
 module.exports = {
     development: {
